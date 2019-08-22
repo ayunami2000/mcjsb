596f0c900e2b1f99e2db6d188ab965a75d915a1d
mcjsb <54388779+mcjsb@users.noreply.github.com>
2019-08-22 14:40:24 +0800

    Update craft.js

--- craft.js
+++ craft.js
@@ -36,6 +36,60 @@ if(window.navigator.userAgent.search("Firefox")!=-1) {
 Math.radians = function(degrees) { return degrees * Math.PI / 180; };
 Math.degrees = function(radians) { return radians * 180 / Math.PI; };
 
+var fps = 0;
+
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame   || 
+    window.mozRequestAnimationFrame      || 
+    window.oRequestAnimationFrame        || 
+    window.msRequestAnimationFrame       || 
+    function(callback, element){
+        window.setTimeout(function(){
+           
+            callback(+new Date);
+        }, 1000 / 60);
+    };
+})();
+
+var lastRun;
+(function(window, document){
+
+    var canvas       = document.getElementById("webby-box"),
+        context      = canvas.getContext("2d"),
+        width        = canvas.width,
+        height       = canvas.height,
+        game_running = true,
+        show_fps     = true;
+
+    function showFPS(){
+        context.fillStyle = "Black";
+        context.font      = "normal 16pt Arial";
+
+        context.fillText(fps + " fps", 10, 26);
+    }
+    function gameLoop(){
+        if(!lastRun) {
+            lastRun = new Date().getTime();
+            requestAnimFrame(gameLoop);
+            return;
+        }
+        var delta = (new Date().getTime() - lastRun)/1000;
+        lastRun = new Date().getTime();
+        fps = 1/delta;
+        //Clear screen
+        context.clearRect(0, 0, width, height);
+
+        if (show_fps) showFPS();
+
+        if (game_running) requestAnimFrame(gameLoop);
+
+    }
+    
+    gameLoop();
+
+}(this, this.document))
+
 /// ############# ///       /// ############# ///
 /// #### /// #### /// WEBGL /// #### /// #### ///
 /// ############# ///       /// ############# ///
